f0f39f22b74c5730c7e886b72eb602536ed13445
Danilo Tuler <tuler@pobox.com>
2023-10-11 23:19:08 -0400

    Cartesi machine with engine to run in the browser

---
+++ browser-machine/src/index.ts
@@ -0,0 +1,12 @@
+import { createApp } from "@deroll/app";
+import { toHex } from "viem";
+
+const app = createApp({ url: "http://127.0.0.1:5004" });
+
+app.addAdvanceHandler(async (data) => {
+  console.log(JSON.stringify(data.metadata));
+  app.createNotice({ payload: toHex("hello") });
+  return "accept";
+});
+
+app.start().catch((e) => process.exit(1));
